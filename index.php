ea8524e356b7d257c21eb9e51526d0e0bcf578d5
Anas Mazouni <madadj4@gmail.com>
2017-05-08 13:28:13 +0100

    More system info added !

--- index.php
+++ index.php
@@ -16,6 +16,18 @@
 			}
     }
   }
+  $rootDir = opendir("."); // open current directory
+  $dirArray = array();
+  // get each entry
+  while($element = readdir($rootDir)) {
+    // exclude files and hidden folders
+    if (is_dir($element) && (substr($element, 0, 1) != ".")) {
+      $dirArray[] = $element;
+    }
+  }
+  closedir($rootDir);                         // close directory
+  $projectCount = count($dirArray) ;          // count elements in array
+  if ($projectCount > 0) { sort($dirArray); } // sort 'em
 ?>
     <!DOCTYPE html>
     <html lang="en">
@@ -57,71 +69,75 @@
 
             </div>
         </div>
-
-        <?php
-        $rootDir = opendir("."); // open current directory
-        $dirArray = array();
-        // get each entry
-        while($element = readdir($rootDir)) {
-          // exclude files and hidden folders
-          if (is_dir($element) && (substr($element, 0, 1) != ".")) {
-            $dirArray[] = $element;
-          }
-        }
-        closedir($rootDir);                         // close directory
-        $projectCount = count($dirArray) ;          // count elements in array
-        if ($projectCount > 0) { sort($dirArray); } // sort 'em
-        ?>
-            <div class="container">
-                <div class="section">
-                    <div class="row">
-                        <div class="card">
-                            <div class="card-content">
-                                <h4>Server Summary : <b><?= $_SERVER['SERVER_NAME']; ?></b></h4>
+        <div class="container">
+            <div class="section">
+                <div class="row">
+                    <div class="card">
+                        <div class="card-content">
+                            <h4>Server Summary : <b><?= $_SERVER['SERVER_NAME']; ?></b></h4>
+                        </div>
+                        <div class="card-tabs">
+                            <ul class="tabs tabs-fixed-width">
+                                <li class="tab"><a class="purple-text text-lighten-3" href="#soft">Server Software</a></li>
+                                <li class="tab"><a class="purple-text text-lighten-3 active" href="#php">PHP Version</a></li>
+                                <li class="tab"><a class="purple-text text-lighten-3" href="#root">Projects (<b><?php echo ($projectCount > 0)? $projectCount : "No"; ?></b>) </a></li>
+                            </ul>
+                        </div>
+                        <div class="card-content grey lighten-4">
+                            <div id="soft">
+                                <?= $_SERVER['SERVER_SOFTWARE']; ?>
+                                <hr>
+                                <?php
+                                echo "<table border = '2' style='color:#ffffff;'>";
+                                $i = 0;
+                                foreach($_SERVER as $key=>$value){
+                                if($i%2 == 0) {
+                                echo "<tr bgcolor='#993300'>";
+                                } else {
+                                echo "<tr bgcolor='#0099FF'>";
+                                }
+                                echo "<td>";
+                                echo $key;
+                                echo "</td>";
+                                echo "<td>";
+                                echo $value;
+                                echo "</td>";
+                                echo "</tr>";
+                                $i++;
+                                }
+                                echo "</table>";
+                                 ?>
                             </div>
-                            <div class="card-tabs">
-                                <ul class="tabs tabs-fixed-width">
-                                    <li class="tab"><a class="purple-text text-lighten-3" href="#soft">Server Software</a></li>
-                                    <li class="tab"><a class="purple-text text-lighten-3 active" href="#php">PHP Version</a></li>
-                                    <li class="tab"><a class="purple-text text-lighten-3" href="#root">Projects (<b><?php echo ($projectCount > 0)? $projectCount : "No"; ?></b>) </a></li>
+                            <div id="php">
+                                <ul class="collection">
+
+                                    <li class="collection-item avatar">
+                                        <i class="devicons devicons-php circle deep-purple"></i>
+                                        <span class="title">PHP version</span>
+                                        <p><span>Check <a title="phpinfo()" target="_blank" href="/?q=info">info</a></span> </p>
+                                        <a href="#!" class="secondary-content"><span class="new badge" data-badge-caption="<?= phpversion(); ?>">V</span></a>
+                                    </li>
+
                                 </ul>
-                            </div>
-                            <div class="card-content grey lighten-4">
-                                <div id="soft">
-                                    <?php print($_SERVER['SERVER_SOFTWARE']); ?>
-                                </div>
-                                <div id="php">
-                                    <ul class="collection">
-
-                                        <li class="collection-item avatar">
-                                            <i class="devicons devicons-php circle deep-purple"></i>
-                                            <span class="title">PHP version</span>
-                                            <p><span>Check <a title="phpinfo()" target="_blank" href="/?q=info">info</a></span> </p>
-                                            <a href="#!" class="secondary-content"><span class="new badge" data-badge-caption="<?php print phpversion(); ?>">V</span></a>
-                                        </li>
-
-                                    </ul>
-                                    <ul class="collapsible" data-collapsible="accordion">
-                                        <li>
-                                            <div class="collapsible-header"><i class="material-icons">explicit</i> PHP Extensions</div>
-                                            <div class="collapsible-body">
-                                                <ul>
-                                                    <?php
-                                          $extensions = get_loaded_extensions();
-                                          natcasesort($extensions);
-                                          foreach($extensions as $extention):
-                                          ?>
-                                                        <li>
-                                                            <?= $extention; ?>
-                                                        </li>
-                                                        <?php endforeach; ?>
-                                                </ul>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <div class="collapsible-header"><i class="material-icons">settings</i> INI Settings</div>
-                                            <div class="collapsible-body">
+                                <ul class="collapsible" data-collapsible="accordion">
+                                    <li>
+                                        <div class="collapsible-header"><i class="material-icons">explicit</i> PHP Extensions</div>
+                                        <div class="collapsible-body">
+                                            <ul>
                                                 <?php
+                                                  $extensions = get_loaded_extensions();
+                                                  natcasesort($extensions);
+                                                  foreach($extensions as $extention):
+                                                ?>
+                                                    <li><?= $extention; ?></li>
+                                                <?php endforeach; ?>
+                                            </ul>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="collapsible-header"><i class="material-icons">settings</i> INI Settings</div>
+                                        <div class="collapsible-body">
+                                            <?php
                                               $inipath = php_ini_loaded_file();
                                               if ($inipath) {
                                                   echo '<p>Loaded php.ini: <a href="'.$inipath.'" target="_blank">'.$inipath.'</a></p>';
@@ -133,66 +149,61 @@
                                               echo '- Post_max_size = ' . ini_get('post_max_size') . "<br />";
                                               echo '- Upload_max_filesize = ' . ini_get('upload_max_filesize');
                                            ?>
-                                            </div>
-                                        </li>
+                                        </div>
+                                    </li>
 
-                                    </ul>
-                                </div>
-                                <div id="root">Document Root: <b><?php print ($_SERVER['DOCUMENT_ROOT']); ?></b><br />
-                                    <ul class="collection">
-                                        <?php if ($projectCount > 0) : ?>
-                                        <?php foreach ($dirArray as $dir):
-                                          $modtime=date("M j, Y, g:i A", filemtime($dir));
-                                          ?>
-                                        <li class="collection-item avatar">
-                                          <?php if(get_icon($dir) != null){?>
-                                            <img src="<?= get_icon($dir); ?>" alt="" class="circle">
-                                          <?php }else{ ?>
-                                            <i class="material-icons circle">folder</i>
-                                          <?php } ?>
-                                            <span class="title"><?php echo $dir ;?></span>
-                                            <p>Last mofied at :
-                                                <?= $modtime; ?>
-                                            </p>
-                                            <a href="http://localhost/<?php echo $dir ;?>" target="_blank" class="secondary-content"><i class="material-icons">open_in_new</i></a>
-                                        </li>
-
-                                        <?php endforeach; ?>
-                                        <?php else: ?>
-                                    </ul>
-
-
-                                    <?php endif; ?>
-
-                                </div>
+                                </ul>
+                            </div>
+                            <div id="root">Document Root: <b><?= $_SERVER['DOCUMENT_ROOT']); ?></b><br />
+                                <ul class="collection">
+                                    <?php
+                                    if ($projectCount > 0) :
+                                      foreach ($dirArray as $dir):
+                                        $modtime=date("M j, Y, g:i A", filemtime($dir));
+                                    ?>
+                                    <li class="collection-item avatar">
+                                        <?php if(get_icon($dir) != null){?>
+                                        <img src="<?= get_icon($dir); ?>" alt="" class="circle">
+                                        <?php }else{ ?>
+                                        <i class="material-icons circle">folder</i>
+                                        <?php } ?>
+                                        <span class="title"><?= $dir ;?></span>
+                                        <p>Last mofied at : <b><?= $modtime; ?></b></p>
+                                        <a href="http://localhost/<?= $dir ;?>" target="_blank" class="secondary-content"><i class="material-icons">open_in_new</i></a>
+                                    </li>
+                                    <?php endforeach; ?>
+                                    <?php else: ?>
+                                </ul>
+                                <?php endif; ?>
                             </div>
                         </div>
-
                     </div>
-                </div>
-
-
-
-                <br><br>
-
-                <div class="section">
 
                 </div>
             </div>
 
-            <footer class="page-footer green accent-2">
 
 
-                <div class="footer-copyright">
-                    <div class="container">
-                        Made by <a class="purple-text text-lighten-3" href="https://stormix.co">Stormix</a>
-                    </div>
+            <br><br>
+
+            <div class="section">
+
+            </div>
+        </div>
+
+        <footer class="page-footer green accent-2">
+
+
+            <div class="footer-copyright">
+                <div class="container">
+                    Made by <a class="purple-text text-lighten-3" href="https://stormix.co">Stormix</a>
                 </div>
-            </footer>
+            </div>
+        </footer>
 
-            <!--Import jQuery before materialize.js-->
-            <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
+        <!--Import jQuery before materialize.js-->
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
 
     </body>
 
